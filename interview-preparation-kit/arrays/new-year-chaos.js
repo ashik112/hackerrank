5bf56897079bfd2b17e41277cd0bca02d9f2e446
md.ashekur <ashekur.rahman@eongroup.net.bd>
2019-03-24 18:05:03 +0600

    new year chaos solved with explanation & samples

--- interview-preparation-kit/arrays/new-year-chaos.js
+++ interview-preparation-kit/arrays/new-year-chaos.js
@@ -7,12 +7,14 @@ function minimumBribes(q) {
       console.log("Too chaotic");
       return;
     }
+    // to calculate number of bribes received by one person,
+    // only needs to check how many numbers are greater than q[i] from the initial one position before  to current position
+    // here Math.max is used incase, the value of (q[i]-2) is less than 0
     for (let j = i - 1; j >= Math.max(0, q[i] - 2); j--) {
       if (q[j] > q[i]) bribes++;
     }
   }
-
-  //console.log();
   console.log(bribes);
-  //console.log();
 }
+minimumBribes([2, 5, 1, 3, 4]); // Too Chaotic
+minimumBribes([1, 2, 5, 3, 7, 8, 6, 4]); // 7
