7978682647c58e1d6b5a6230af1f1d0f5853505a
ashik112 <md.ashekur@gmail.com>
2019-03-26 04:41:37 +0600

    solve Minimum Absolute Difference in An Array using greedy algorithm

--- interview-preparation-kit/greedy-algorithms/minimum-absoulte-difference-in-an-array.js
+++ interview-preparation-kit/greedy-algorithms/minimum-absoulte-difference-in-an-array.js
@@ -12,6 +12,6 @@ function minimumAbsoluteDifference(arr) {
 function main(){
     console.log(minimumAbsoluteDifference([3,-7,0])); // 3
     console.log(minimumAbsoluteDifference([-59 ,-36, -13, 1 ,-53, -92, -2, -96, -54, 75])); // 1
-    console.log(minimumAbsoluteDifference([1, -3, 71, 68, 17])); // 1
+    console.log(minimumAbsoluteDifference([1, -3, 71, 68, 17])); // 3
 }
 main();
