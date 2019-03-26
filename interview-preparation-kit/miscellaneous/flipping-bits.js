65946dff24081981ad08bb13588c0343df64dde3
ashik112 <md.ashekur@gmail.com>
2019-03-27 03:58:00 +0600

    solve Flipping Bits

---
+++ interview-preparation-kit/miscellaneous/flipping-bits.js
@@ -0,0 +1,12 @@
+//Flipping bits
+
+function flippingBits(N) {
+    /* The only JavaScript operator that works using unsigned 32-bit integers is >>>. You can        
+     * exploit this to convert a signed-integer-in-Number.
+     */
+    return (~N) >>> 0;
+}
+
+console.log(flippingBits(2147483647)); // 2147483648
+console.log(flippingBits(1)); // 4294967294
+console.log(flippingBits(0)); // 4294967295
